ec8c1176f87f7cffeefc65796a28430f42c4a998
Julian <167174811+JBTastic@users.noreply.github.com>
2024-12-22 16:43:21 +0100

    added solution for TwoSum in Python3

---
+++ Two-Sum/two-sum.py
@@ -0,0 +1,19 @@
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        hashmap = {}
+        for i, num in enumerate(nums) :
+            difference = target - num
+            if difference in hashmap :
+                return [hashmap[difference], i]
+            hashmap[num] = i
+        return []
+        
+
+
+
+
+
+
+print("True" if Solution().twoSum([2,7,11,15], 13) == [0,2] else "False")
+print("True" if Solution().twoSum([3,2,4], 6) == [1,2] else "False")
+print("True" if Solution().twoSum([3,3], 6) == [0,1] else "False")
