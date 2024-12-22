7af97d2a805310e7c25007cc8b8ff4bcae97762c
Julian <167174811+JBTastic@users.noreply.github.com>
2024-12-22 18:13:42 +0100

    fixed a few problems by renaming the Kotlin file

--- Two-Sum/two-sum.py
+++ Two-Sum/two-sum.py
@@ -10,10 +10,6 @@ class Solution:
         
 
 
-
-
-
-
 print("True" if Solution().twoSum([2,7,11,15], 13) == [0,2] else "False")
 print("True" if Solution().twoSum([3,2,4], 6) == [1,2] else "False")
 print("True" if Solution().twoSum([3,3], 6) == [0,1] else "False")
