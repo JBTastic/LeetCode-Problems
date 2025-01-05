3e04207021c757d5293beec88fd2edca6fa00448
Julian <167174811+JBTastic@users.noreply.github.com>
2025-01-05 21:05:25 +0100

    --- Auto Git Commit ---

--- 1medium/count-almost-equal-pairs-1/solution.kt
+++ 1medium/count-almost-equal-pairs-1/solution.kt
@@ -1,6 +1,35 @@
 class Solution {
+    fun compare(a: Int, b: Int): Boolean {
+        println("Comparing $a and $b")
+
+        // both numbers are already equal
+        if (a == b) return true
+
+        // both numbers are almost equal
+        val aDigits = a.toString().toList()
+        val bDigits = b.toString().toList()
+
+        //check if both numbers have equal number of digits
+        if (aDigits.size != bDigits.size) return false
+
+        //check in which position the numbers differ
+        val differentIndices = aDigits.indices.filter { aDigits[it] != bDigits[it] }
+
+        // if exactly two digits differ, check if switching them will make them equal
+        return differentIndices.size == 2 && aDigits[differentIndices[0]] == bDigits[differentIndices[1]] && aDigits[differentIndices[1]] == bDigits[differentIndices[0]]
+    }
+
+
     fun countPairs(nums: IntArray): Int {
-        return -1
+        var counter = 0
+        for (i in 0 until nums.size - 1) {
+            for (j in i + 1 until nums.size) {
+                if (compare(nums[i], nums[j])) {
+                    counter++
+                }
+            }
+        }
+        return counter
     }
 }
 
