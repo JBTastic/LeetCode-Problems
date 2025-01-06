acb499f0544f103af4519bf3146bca5f3be5c4b4
Julian <167174811+JBTastic@users.noreply.github.com>
2025-01-06 19:35:29 +0100

    --- Auto Git Commit ---

--- 1medium/my-calender-2/solution.kt
+++ 1medium/my-calender-2/solution.kt
@@ -1,19 +1,22 @@
 class MyCalendarTwo() {
 
     // create a mutable list of bookings
-    private val bookings: MutableList<Triple<Int, Int, Int>> = mutableListOf()
+    var bookings: MutableList<Triple<Int, Int, Int>> = mutableListOf()
 
     fun book(startTime: Int, endTime: Int): Boolean {
 
         // check for every new booking if it overlaps with an existing booking
         for ((start2, end2, doubleBooking) in bookings) {
-            if (startTime < end2 && endTime > start2 && doubleBooking > 0) {
+            if (startTime < end2 && endTime > start2 && doubleBooking > 1) {
                 return false
             } else if (startTime < end2 && endTime > start2 && doubleBooking == 0) {
                 bookings.add(Triple(startTime, endTime, 1))
                 bookings.remove(Triple(start2, end2, 0))
                 bookings.add(Triple(start2, end2, 1))
+                bookings.add(Triple(start2, endTime, 2))
                 return true
+            } else if (startTime < end2 && endTime > start2 && doubleBooking == 1) {
+                
             }
         }
         
@@ -25,13 +28,30 @@ class MyCalendarTwo() {
 
 fun main () {
     val obj = MyCalendarTwo()
-    val testCase = [[47,50],[1,10],[27,36],[40,47],[20,27],[15,23],[10,18],[27,36],[17,25],[8,17],[24,33],[23,28],[21,27],[47,50],[14,21],[26,32],[16,21],[2,7],[24,33],[6,13],[44,50],[33,39],[30,36],[6,15],[21,27],[49,50],[38,45],[4,12],[46,50],[13,21]]
 
-    val expected = [true,true,true,true,true,true,true,true,false,false,false,false,false,true,false,false,false,true,false,false,false,false,false,false,false,false,true,false,false,false]
+    val testCase: List<List<Int>> = listOf(
+        listOf(47,50), listOf(1,10), listOf(27,36), listOf(40,47),
+        listOf(20,27), listOf(15,23), listOf(10,18), listOf(27,36),
+        listOf(17,25), listOf(8,17), listOf(24,33), listOf(23,28),
+        listOf(21,27), listOf(47,50), listOf(14,21), listOf(26,32),
+        listOf(16,21), listOf(2,7), listOf(24,33), listOf(6,13),
+        listOf(44,50), listOf(33,39), listOf(30,36), listOf(6,15),
+        listOf(21,27), listOf(49,50), listOf(38,45), listOf(4,12),
+        listOf(46,50), listOf(13,21)
+    )
+
+    val expected: List<Boolean> = listOf(
+        true,true,true,true,true,true,true,true,false,false,false,false,
+        false,true,false,false,false,true,false,false,false,false,false,
+        false,false,true,false,false,false
+    )
     
-    for (i in testCase) {
-        println(obj.book(i[0], i[1]))
+    for (i in testCase.indices) {
+        val result = obj.book(testCase[i][0], testCase[i][1])
+        require(result == expected[i]) { "Expected ${expected[i]}, but got $result for test case ${testCase[i]}, also bookings is ${obj.bookings}" }
+        println("Test ${testCase[i]} passed")
     }
+}
 
 /*
  * Your MyCalendar object will be instantiated and called as such:
