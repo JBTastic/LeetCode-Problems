4d6c452487d0a35bee12e46fdebf77b8921ae2a5
Julian <167174811+JBTastic@users.noreply.github.com>
2025-01-12 17:24:06 +0100

    added solution for easy question 1893

---
+++ 0easy/1893-check-if-all-the-integers-in-an-array-are-covered/solution.kt
@@ -0,0 +1,24 @@
+class Solution {
+    fun isCovered(ranges: Array<IntArray>, left: Int, right: Int): Boolean {
+        var leftToRight= mutableListOf<Int>()
+
+        for (i in left until right +1) {leftToRight.add(i)}
+        for (range in ranges) {
+            for (j in range[0]..range[1]) {
+                leftToRight.remove(j)
+            }
+        }
+        if (leftToRight.isEmpty()) {return true} else {return false}
+    }
+}
+
+fun main() {
+    val obj1 = Solution()
+
+    val ranges1 = arrayOf(intArrayOf(1, 2), intArrayOf(3, 4), intArrayOf(5, 6))
+    val left1 = 2
+    val right1 = 5
+
+    require(obj1.isCovered(ranges1, left1, right1) == true) {"Expected true, but got ${obj1.isCovered(ranges1, left1, right1)}"}
+    println("Test passed")
+}
