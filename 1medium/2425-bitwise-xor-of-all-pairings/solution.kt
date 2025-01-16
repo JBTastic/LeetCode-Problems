7e5269dfa92d947c5fe53882ffcd66bc39e860e2
Julian <167174811+JBTastic@users.noreply.github.com>
2025-01-16 15:29:58 +0100

    added Kotlin solution for LeetCode problem: 2425 bitwise xor of all pairings

---
+++ 1medium/2425-bitwise-xor-of-all-pairings/solution.kt
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/bitwise-xor-of-all-pairings/?envType=daily-question&envId=2025-01-16
+
+class Solution {
+    fun xorAllNums(nums1: IntArray, nums2: IntArray): Int {
+        val nums1Size = nums1.size
+        val nums2Size = nums2.size
+
+        // since xor is associative and commutative
+        if (nums1Size % 2 == 0 && nums2Size % 2 == 0) {
+            return 0
+        } else if (nums1Size % 2 == 0){
+            return nums1.reduce { acc, i -> acc xor i }
+        } else if (nums2Size % 2 == 0) {
+            return nums2.reduce { acc, i -> acc xor i }
+        } else {
+            return nums1.reduce { acc, i -> acc xor i } xor nums2.reduce { acc, i -> acc xor i }
+        }
+    }
+}
+
+fun main() {
+    val obj = Solution()
+    val nums1 = intArrayOf(2,1,3)
+    val nums2 = intArrayOf(10,2,5,0)
+    val result = obj.xorAllNums(nums1, nums2)
+    require(result == 13) { "Expected 13, but got $result" }
+    println(result) // Output: 13
+}
