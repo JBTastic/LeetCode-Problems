6112eefe9820457a4469c9f4bef5b0ac60384995
Julian <167174811+JBTastic@users.noreply.github.com>
2025-10-21 12:30:34 +0200

    Time limit exceeded

--- 0easy/0121-best-time-to-buy-and-sell-stock/solution.py
+++ 0easy/0121-best-time-to-buy-and-sell-stock/solution.py
@@ -5,13 +5,24 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        pass
+        # Iterate through the list of prices with two pointers
+        result = 0
+        for i in range(len(prices) - 1):
+            for j in range(i+1, len(prices)):
+                if prices[j] > prices[i]:
+                    tmp = prices[j] - prices[i]
+                    if tmp > result:
+                        result = tmp
+        return result
+                
 
 
 def main():
     s = Solution()
     result = s.maxProfit([7, 1, 5, 3, 6, 4])
+    result2 = s.maxProfit([7,6,4,3,1])
     print(result)  # Expected output: 5
+    print(result2) # Expected output: 0
 
 if __name__ == "__main__":
     main()
