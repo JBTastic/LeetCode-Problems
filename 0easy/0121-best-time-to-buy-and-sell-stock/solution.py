ee337fda0af50a1a63ed5992e14258245895142b
Julian <167174811+JBTastic@users.noreply.github.com>
2025-10-21 12:23:09 +0200

    Trying LeetCode problem 121

---
+++ 0easy/0121-best-time-to-buy-and-sell-stock/solution.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        pass
+
+
+def main():
+    s = Solution()
+    result = s.maxProfit([7, 1, 5, 3, 6, 4])
+    print(result)  # Expected output: 5
+
+if __name__ == "__main__":
+    main()
